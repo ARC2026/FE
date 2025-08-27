939bcd6b78028acbddc84d37916a5ba338d08fa7
nyun-nye <2001_11_7@naver.com>
2025-08-27 12:12:45 +0900

    🌱: 프로젝트 초기 세팅

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.scss";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
