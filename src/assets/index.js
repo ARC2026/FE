c97e16dbea6099557f4f7ece9456c568f111cac2
nyun-nye <2001_11_7@naver.com>
2025-10-13 18:16:20 +0900

    ✨: home 아이콘 추가

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,5 +3,6 @@ import bracketsLeft from "./brackets_left.svg";
 import bracketsRight from "./brackets_right.svg";
 import date from "./date.svg";
 import menuBackground from "./menu_background.svg";
+import home from "./home.svg";
 
-export { background, bracketsLeft, bracketsRight, date, menuBackground };
+export { background, bracketsLeft, bracketsRight, date, menuBackground, home };
