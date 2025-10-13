e665de2730ef3ca5a36c5f6671240ac0e3f6f584
nyun-nye <2001_11_7@naver.com>
2025-10-13 18:08:04 +0900

    ✨: 메뉴별 페이지 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import About from "./pages/About";
+import Project from "./pages/Project";
+import Map from "./pages/Map";
 
 function App() {
   return (
@@ -7,7 +10,9 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          {/* <Route path="/about" element={<About />} /> */}
+          <Route path="/about" element={<About />} />
+          <Route path="/project" element={<Project />} />
+          <Route path="/map" element={<Map />} />
         </Routes>
       </BrowserRouter>
     </>
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,19 +1,27 @@
 import styles from "../styles/components/Menu.module.scss";
 import Arrow from "./Arrow";
+import { useNavigate } from "react-router-dom";
 export default function Menu() {
+  const navigate = useNavigate();
   return (
     <div className={styles.container}>
       <div className={styles.aboutContainer}>
         <Arrow color="#FF3435" />
-        <div className={styles.about}>ABOUT</div>
+        <div className={styles.about} onClick={() => navigate("/about")}>
+          ABOUT
+        </div>
       </div>
       <div className={styles.projectContainer}>
         <Arrow color="#038DCC" />
-        <div className={styles.project}>PROJECT</div>
+        <div className={styles.project} onClick={() => navigate("/project")}>
+          PROJECT
+        </div>
       </div>
       <div className={styles.mapContainer}>
         <Arrow color="#B6DA03" />
-        <div className={styles.map}>MAP</div>
+        <div className={styles.map} onClick={() => navigate("/map")}>
+          MAP
+        </div>
       </div>
     </div>
   );
