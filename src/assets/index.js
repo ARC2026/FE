0405a6a9649aa6a6943b8d9afc98822f1c0b4a48
nyun-nye <2001_11_7@naver.com>
2025-10-13 21:13:08 +0900

    ✨: 소괄호 아이콘 추가

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,5 +4,16 @@ import bracketsRight from "./brackets_right.svg";
 import date from "./date.svg";
 import menuBackground from "./menu_background.svg";
 import home from "./home.svg";
+import parenthesisLeft from "./parenthesis_left.svg";
+import parenthesisRight from "./parenthesis_right.svg";
 
-export { background, bracketsLeft, bracketsRight, date, menuBackground, home };
+export {
+  background,
+  bracketsLeft,
+  bracketsRight,
+  date,
+  menuBackground,
+  home,
+  parenthesisLeft,
+  parenthesisRight,
+};
