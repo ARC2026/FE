0202edbcee0822e575cba240bec2b603759129f5
nyun-nye <2001_11_7@naver.com>
2025-12-30 14:40:18 +0900

    ♻️: about 페이지 리팩토링

--- src/pages/Temp.jsx
+++ src/pages/Temp.jsx
@@ -1,8 +1,9 @@
-import ProjectContainer from "../components/ProjectContainer";
+import AboutCard from "../components/ProfileCard";
+import { profile } from "../assets";
 export default function Temp() {
   return (
-    <div>
-      <ProjectContainer />
+    <div style={{ backgroundColor: "#1c1f1c" }}>
+      <AboutCard position="OFFICE" img={profile} name="오지샘" />
     </div>
   );
 }
