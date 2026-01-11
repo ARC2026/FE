d83b27831312e02cdaadc7e1ec4208df39ff3cad
nyun-nye <2001_11_7@naver.com>
2026-01-11 22:40:17 +0900

    ♻️ : 홈 페이지 리펙토링

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,21 +2,21 @@ import background from "./background.svg";
 import bracketsLeft from "./brackets_left.svg";
 import bracketsRight from "./brackets_right.svg";
 import date from "./date.svg";
-import menuBackground from "./menu_background.svg";
 import home from "./home.svg";
 import parenthesisLeft from "./parenthesis_left.svg";
 import parenthesisRight from "./parenthesis_right.svg";
 import profile from "./profile.png";
 import projectSample from "./project_sample.png";
+import fragments from "./fragments.svg";
 export {
   background,
   bracketsLeft,
   bracketsRight,
   date,
-  menuBackground,
   home,
   parenthesisLeft,
   parenthesisRight,
   profile,
   projectSample,
+  fragments,
 };
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,26 +1,39 @@
 import styles from "../styles/components/Menu.module.scss";
 import Arrow from "./Arrow";
 import { useNavigate } from "react-router-dom";
+import { home } from "../assets";
 export default function Menu() {
   const navigate = useNavigate();
   return (
     <div className={styles.container}>
-      <div className={styles.aboutContainer}>
-        <Arrow color="#FF3435" />
-        <div className={styles.about} onClick={() => navigate("/about")}>
-          ABOUT
-        </div>
+      <div className={styles.homeContainer} onClick={() => navigate("/")}>
+        <img src={home} alt="home" className={styles.homeIcon} />
+        <div className={styles.home}>HOME</div>
       </div>
-      <div className={styles.projectContainer}>
-        <Arrow color="#038DCC" />
-        <div className={styles.project} onClick={() => navigate("/project")}>
-          PROJECT
+      <div className={styles.menuContainer}>
+        <div
+          className={styles.aboutContainer}
+          onClick={() => navigate("/about")}
+        >
+          <Arrow color="#FF3435" width={12} height={12} />
+          <div className={styles.about} onClick={() => navigate("/about")}>
+            ABOUT
+          </div>
         </div>
-      </div>
-      <div className={styles.mapContainer}>
-        <Arrow color="#B6DA03" />
-        <div className={styles.map} onClick={() => navigate("/map")}>
-          MAP
+        <div
+          className={styles.projectContainer}
+          onClick={() => navigate("/project")}
+        >
+          <Arrow color="#038DCC" width={12} height={12} />
+          <div className={styles.project} onClick={() => navigate("/project")}>
+            PROJECT
+          </div>
+        </div>
+        <div className={styles.mapContainer} onClick={() => navigate("/map")}>
+          <Arrow color="#B6DA03" width={12} height={12} />
+          <div className={styles.map} onClick={() => navigate("/map")}>
+            MAP
+          </div>
         </div>
       </div>
     </div>
