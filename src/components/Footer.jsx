9bba4f7f6f75569a2ba979583923ca0e9ffa01d5
hardwoong <dew04300@naver.com>
2026-01-22 00:12:38 +0900

    ♻️: 푸터 수정

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import styles from "../styles/components/Footer.module.scss";
+import { rixfont, photomate } from "../assets";
 
 export default function Footer() {
   return (
@@ -12,16 +13,24 @@ export default function Footer() {
           <div className={styles.footerText}>
             서울특별시 종로구 인사동 5길 14
           </div>
-          <div className={styles.footerText}>마루아트센터 지하1층</div>
-          <div className={styles.footerText}>@arc_project</div>
+          <div className={styles.footerText}>전시 협찬</div>
+          <a 
+            href="https://www.instagram.com/arc__project/" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            className={styles.footerLink}
+          >
+            @arc__project
+          </a>
         </div>
         <div className={styles.footerRight}>
           <div className={styles.footerText}>ART & DESIGN EXHIBITION</div>
           <div className={styles.footerText}>
             SEOUL JONGNO-GU INSADONG 5-GIL 14,
           </div>
-          <div className={styles.footerText}>
-            MARU ART CENTER B1F SPECIAL EXHIBITION HALL
+          <div className={styles.sponsorLogos}>
+            <img src={rixfont} alt="RixFont" />
+            <img src={photomate} alt="Photomate" />
           </div>
           <div className={styles.footerText}>
             © 2026. ARC All rights reserved.
